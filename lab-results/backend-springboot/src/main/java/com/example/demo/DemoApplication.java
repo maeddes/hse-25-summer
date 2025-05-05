9c9616beb30793605d432ab1ca7f38341b393bfa
Matthias Haeussler <maeddes@gmail.com>
2025-05-05 09:10:02 +0000

    Updated App and Compose for Scalability

--- lab-results/backend-springboot/src/main/java/com/example/demo/DemoApplication.java
+++ lab-results/backend-springboot/src/main/java/com/example/demo/DemoApplication.java
@@ -1,5 +1,8 @@
 package com.example.demo;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.*;
@@ -7,6 +10,17 @@ import org.springframework.web.bind.annotation.*;
 @SpringBootApplication
 @RestController
 public class DemoApplication {
+
+	@GetMapping("/hostname")
+	public String getHostname() {
+		String hostname;
+		try {
+			hostname = InetAddress.getLocalHost().getHostName();
+		} catch (UnknownHostException e) {
+			hostname = "Unknown";
+		}
+		return hostname;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
